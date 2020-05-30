815f3db02977bc8b24690c2e2cef5d02c7b67bc7
yoky.artistic@hotmail.com <yoky.artistic@hotmail.com>
2020-05-31 09:52:15 +1000

    TEST

---
+++ A2 Implement a comparator/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace A2_Implement_a_comparator
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(458, 90);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(46, 17);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "label1";
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 16;
+            this.lstBox.Location = new System.Drawing.Point(461, 110);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(120, 84);
+            this.lstBox.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(1106, 472);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.lblText);
+            this.Name = "FrmMain";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.ListBox lstBox;
+    }
+}
+
---
+++ A2 Implement a comparator/Form1.cs
@@ -0,0 +1,134 @@
+﻿
+//*****************************************************************************
+// Programmer: Kamolchanok Pornleesangsuwan
+// Date: 24 May 2020
+// Software: Microsoft Visual Studio 2019 Community Edition
+// Platform: Microsoft Windows 10 Professional 64­bit
+// Purpose: A2_Implement_a_comparator
+// Adapted from https://www.jonasjohn.de/snippets/csharp/drag­anddrop­example.htm
+//
+//*****************************************************************************
+
+
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A2_Implement_a_comparator
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        public class ReverserClass : IComparer
+        {
+            // Call CaseInsensitiveComparer.Compare with the parameters reversed.
+            int IComparer.Compare(Object x, Object y)
+            {
+                return ((new CaseInsensitiveComparer()).Compare(y, x));
+            }
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+           
+                // Initialize a string array.
+                string [] words = { "The", "quick", "brown", "fox", "jumps", "over",
+                         "the", "lazy", "dog" };
+
+            // Display the array values.
+            lblText.Text = "The array initially contains the following values";
+            // lstBox.Text = PrintIndexAndValues(words);
+
+            //PrintIndexAndValues(words));
+
+            // Sort the array values using the default comparer.
+            Array.Sort(words);
+            Console.WriteLine("After sorting with the default comparer:");
+            PrintIndexAndValues(words);
+
+            // Sort the array values using the reverse case-insensitive comparer.
+            Array.Sort(words, new ReverserClass());
+            Console.WriteLine("After sorting with the reverse case-insensitive comparer:");
+            PrintIndexAndValues(words);
+        }
+
+        
+            public static void PrintIndexAndValues(IEnumerable list)
+            {
+                
+                int i = 0;
+                foreach (var item in list)
+                Console.WriteLine($"   [{i++}]:  {item}");
+
+            }
+        }
+    }
+
+
+
+
+    /*  
+     *   [STAThread]
+            static void Main(string[] args)
+            {
+                // Create an arary of objects.      
+                int fruit[] arrayOfFruits = new fruit[3]
+                 {
+                new fruit("Apple",5),
+                new fruit("Banana",2),
+                new fruit("Grapes",6),
+
+                 };
+
+                // Write out a header for the output.
+                Console.WriteLine("Array - Unsorted\n");
+
+                foreach (fruit f in arrayOfFruits)
+                    Console.WriteLine(f.Type + "\t\t" + f.Price);
+
+                // Demo IComparable by sorting array with "default" sort order.
+                Array.Sort(arrayOfFruits);
+                Console.WriteLine("\nArray - Sorted by Type (Ascending - IComparable)\n");
+
+                foreach (fruit f in arrayOfFruits)
+                    Console.WriteLine(f.Type + "\t\t" + f.Price);
+
+                // Demo ascending sort of numeric value with IComparer.
+                Array.Sort(arrayOfFruits, fruit.sortYearAscending());
+                Console.WriteLine("\nArray - Sorted by Price (Ascending - IComparer)\n");
+
+                foreach (fruit f in arrayOfFruits)
+                    Console.WriteLine(f.Type + "\t\t" + f.Price);
+
+                // Demo descending sort of string value with IComparer.
+                Array.Sort(arrayOfFruits, fruit.sortMakeDescending());
+                Console.WriteLine("\nArray - Sorted by Make (Descending - IComparer)\n");
+
+                foreach (fruit f in arrayOfFruits)
+                    Console.WriteLine(f.Type + "\t\t" + f.Price);
+
+                // Demo descending sort of numeric value using IComparer.
+                Array.Sort(arrayOfFruits, fruit.sortYearDescending());
+                Console.WriteLine("\nArray - Sorted by Price (Descending - IComparer)\n");
+
+                foreach (fruit f in arrayOfFruits)
+                    Console.WriteLine(f.Type + "\t\t" + f.Price);
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    */
+
+
